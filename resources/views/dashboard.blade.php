d76347481822009d3862568fd9956dc5d2312bec
walllys22 <wally.landivar@gmail.com>
2026-05-08 12:00:59 -0400

    Walter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
     <div class="container-fluid">
-        <img src="{{ asset('images/tablero.png') }}" alt="Campeonato" class="img-fluid d-block mx-auto" style="width: 70%;">
+        <img src="{{ asset('images/tablero.png') }}" alt="Campeonato" class="img-fluid d-block mx-auto" style="width: 60%;">
     </div>
 </x-app-layout>
